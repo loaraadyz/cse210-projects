e732722ac8406b8fe74be4cf731e1d84de1580d9
loaraadyz <villal@byui.edu>
2023-09-16 20:36:07 +0800

    csharp prep 3 edited

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //variables
+        int guess_number;
+        int magic_number;
+
+        //for Part 1 and 2
+        //Console.Write("What is the magic number?");
+        //magic_number = int.Parse(Console.ReadLine());
+
+        Random random_generator = new Random();
+        magic_number = random_generator.Next(1,101);
+
+        do
+        {
+        Console.Write("What is the guess number?");
+        guess_number = int.Parse(Console.ReadLine());
+        
+        if (guess_number > magic_number)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guess_number <magic_number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guess it!");
+        }
+        } while (magic_number != guess_number);
+
     }
 }
