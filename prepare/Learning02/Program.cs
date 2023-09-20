963306cc74a2e237bc1cf06b6fd2d8be94de1d57
loaraadyz <villal@byui.edu>
2023-09-20 15:30:42 +0800

    Learning02 finished practice

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle="Instructor I";
+        job1._company = "DMMMSU MLUC";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle="Team Grader";
+        job2._company="BYU-Pathway Worldwide";
+        job2._startYear= 2021;
+        job2._endYear= 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Loara Adyz Villa";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
+
 }
